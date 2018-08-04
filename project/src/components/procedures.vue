f25f4e1a3153d248c7bd9449176b288128ee0489
Nick <niravit_@hotmail.com>
2018-08-05 00:38:19 +1000

    procedures before timer integration

--- project/src/components/procedures.vue
+++ project/src/components/procedures.vue
@@ -12,36 +12,33 @@
             <v-flex xs12 sm6 offset-sm3>
                 <v-card>
 
-                    <v-list v-for="data in myJson" subheader two-line>
+                    <v-list v-for="data in myJson" three-line>
 
-
-                        <v-list-tile@click="">
+                        <v-list-tile @click="">
                             <v-list-tile-action>
-                                <v-checkbox v-model="notifications"></v-checkbox>
+                                <v-checkbox></v-checkbox>
                             </v-list-tile-action>
 
-                            <v-list-tile-content @click="notifications = !notifications">
+                            <v-list-tile-content @click="check = !check">
                                 <v-list-tile-title>{{ data.Task_Description }}</v-list-tile-title>
-
-
-
+                                <v-list-tile-sub-title>{{data.Timer}}</v-list-tile-sub-title>
                             </v-list-tile-content>
-                            </v-list-tile>
+                        </v-list-tile>
+
                     </v-list>
 
-
-
                 </v-card>
+
             </v-flex>
+            <div>
+                <v-btn color="orange darken-2" dark>
+                    <v-icon dark left>arrow_back</v-icon>Back
+                </v-btn>
+            </div>
         </v-layout>
 
     </v-app>
-    <v-layout align-end justify-end row reverse fill-height/>
-    <div>
-        <v-btn color="orange darken-2" dark>
-            <v-icon dark left>arrow_back</v-icon>Back
-        </v-btn>
-    </div>
+
 </div>
 
 </template>
@@ -54,7 +51,7 @@ export default {
     data() {
         return {
             myJson: json,
-            notifications: false
+            check: false
 
         }
     }
