06b8943ac02c039d11bb19a23deee9843afb5438
Nick <niravit_@hotmail.com>
2018-08-04 16:41:57 +1000

    Add modal to patient list

--- project/src/components/patientsList.vue
+++ project/src/components/patientsList.vue
@@ -73,18 +73,90 @@
           </v-layout>
         </v-container>
       </v-card>
+
     </div>
     <v-layout align-end justify-end row fill-height>
-      <v-btn fab dark color="indigo">
-          <v-icon dark>add</v-icon>
-      </v-btn>
+      <v-dialog v-model="dialog" persistent max-width="500px">
+        <v-btn slot="activator" fab dark color="indigo">
+            <v-icon dark>add</v-icon>
+        </v-btn>
+        <v-card>
+          <v-card-title>
+            <span class="headline">Patient Profile</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field label="Legal first name" required></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field
+                    label="Legal last name"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field label="Emergency Contact" required></v-text-field>
+                </v-flex>
+
+                <v-flex xs12 sm6>
+                  <v-text-field label="Age" required></v-text-field>
+                </v-flex>
+
+                <v-flex xs12 sm6>
+                  <v-select
+                    :items="['Male', 'Female', 'Other']"
+                    label="Gender"
+                    required
+                  ></v-select>
+                </v-flex>
+
+                <v-flex xs12 sm6>
+                  <v-text-field label="Height" required></v-text-field>
+                </v-flex>
+
+                <v-flex xs12 sm6>
+                  <v-text-field label="Weight" required></v-text-field>
+                </v-flex>
+
+                <v-flex xs12>
+                  <v-select
+                    :items="['O-Positive', 'O-Negative', 'A-Positive', 'A-Negative', 'B-Positive',
+                     'B-Negative', 'AB-Positive', 'AB-Negative']"
+                    label="Blood Types"
+                    required
+                  ></v-select>
+                </v-flex>
+
+                <v-flex xs12>
+                  <v-text-field label="Address" required></v-text-field>
+                </v-flex>
+
+                <v-flex xs12>
+                  <v-text-field label="Health insurance" required></v-text-field>
+                </v-flex>
+
+
+              </v-layout>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+            <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
     </v-layout>
-    <v-layout align-end justify-end row reverse fill-height/>
-      <div>
-        <v-btn color="orange darken-2" dark>
-            <v-icon dark left>arrow_back</v-icon>Back
-          </v-btn>
-      </div>
+
   </div>
 
 </template>
@@ -96,6 +168,9 @@
 
 <script>
 export default{
-  name: 'patientsList'
+  name: 'patientsList',
+  data: () => ({
+    dialog: false
+  })
 }
 </script>
