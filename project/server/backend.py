75ec0aa1f992df681f94855b08d3375edbbfb3c5
Sam <sam.arche912912@gmail.com>
2018-08-04 16:28:11 +1000

    list to map for objects

--- project/server/backend.py
+++ project/server/backend.py
@@ -6,33 +6,28 @@ app = Flask(__name__)
 CORS(app)
 
 # Data Stores
-patientList = []
-doctorsList = []
+patientList = {}
+doctorsList = {}
 specialtyToPatients = {}
 
 
 # API
 @app.route('/get_patient_list', methods=['GET'])
 def get_patient_list():
+	doctor_id = request.args.get('doctor_id')
+	specialty = 
     return jsonify(patientList)
 
 @app.route('/get_patient', methods=['GET'])
 def get_patient():
     patient_id = request.args.get('patient_id')
-    for patient in patientList:
-        if patient["patient_id"] == patient_id:
-            return jsonify(patient["patient_id"])
-    return jsonify({ "status" : "Failure" })
+    return jsonify(patientList["patient_id"])
 
 @app.route('/add_condition', methods=['GET'])
 def add_condition():
 	patient_id = request.args.get('patient_id')
 	new_condition = request.args.get('condition')
-<<<<<<< Updated upstream
-	for patient in patientList:
-=======
-	for patient in patientList:        #changed paitentList to patientList
->>>>>>> Stashed changes
+	for patient in patientList:        
 		if patient["patient_id"] == patient_id:
 			patient["conditions"].append(new_condition)
 			break
@@ -75,11 +70,10 @@ def add_patient():
 	}
 
 	# Adding the patient to patient list.
-	patientList.append(new_patient)
+	patientList[patient_id] = new_patient
 
 	# Return status. This is arbitary.
 	return jsonify({ "status" : "success" })
-<<<<<<< Updated upstream
 
 @app.route('/add_doctor', methods=['POST'])
 def add_doctor():
@@ -98,12 +92,7 @@ def add_doctor():
 		'specialisation' : specialisation
 	}
 
-	doctorsList.append(new_doctor)
+	doctorsList[doctor_id] = new_doctor
 
 	# Return status. This is arbitary.
 	return jsonify({ "status" : "success" })
-
-
-
-=======
->>>>>>> Stashed changes
